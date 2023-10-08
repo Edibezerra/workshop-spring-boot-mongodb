81068128be8d24bb690c204879a635687c3931ed
Edinardo Bezerra <edinardobezerra@yahoo.com.br>
2023-10-08 15:10:57 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/edibezerra/workshopmongo/domain/Post.java
+++ src/main/java/com/edibezerra/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.edibezerra.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.edibezerra.workshopmongo.dto.AuthorDTO;
+import com.edibezerra.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,8 +24,9 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 
+	private List<CommentDTO> comments = new ArrayList<>();
+
 	public Post() {
-		super();
 	}
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
@@ -72,6 +76,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
--- src/main/java/com/edibezerra/workshopmongo/resource/PostResource.java
+++ src/main/java/com/edibezerra/workshopmongo/resource/PostResource.java
@@ -1,21 +1,13 @@
 package com.edibezerra.workshopmongo.resource;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.edibezerra.workshopmongo.domain.Post;
-import com.edibezerra.workshopmongo.domain.User;
-import com.edibezerra.workshopmongo.dto.UserDTO;
 import com.edibezerra.workshopmongo.services.PostService;
 
 @RestController
