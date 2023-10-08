a83a42aac9b65c6eea659af6bbd19d32415d695e
Edinardo Bezerra <edinardobezerra@yahoo.com.br>
2023-10-08 16:36:41 -0300

    Consulta simples com query methods

--- src/main/java/com/edibezerra/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/edibezerra/workshopmongo/repository/PostRepository.java
@@ -1,12 +1,15 @@
 package com.edibezerra.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.edibezerra.workshopmongo.domain.Post;
-import com.edibezerra.workshopmongo.domain.User;
 
 @Repository
-public interface PostRepository extends MongoRepository<Post, String>{
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
